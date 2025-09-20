411bb82737b67cc6f7b434afe003816d033cd56e
igork <igor.khys@mail.ru>
2025-09-21 00:39:52 +0300

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,17 @@
 
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Lesson1 l1 = new Lesson1();
+        l1.section1();
+        dz1 dz = new dz1();
+        Person person = new Person();
+        person.setAge(12);
+        person.setName("dcw");
+        System.out.println(person.getName());
+        System.out.println(person.getAge());
+        Employee employee = new Employee();
+        employee.setSalary(123);
+        System.out.println(employee.getSalary());
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
     }
 }
