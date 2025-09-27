16956b40746002f15b2279362decf7756c9f310e
igork <igor.khys@mail.ru>
2025-09-27 15:57:20 +0300

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,23 @@
 
 public class Main {
     public static void main(String[] args) {
-        Lesson1 l1 = new Lesson1();
-        l1.section1();
-        dz1 dz = new dz1();
-        Person person = new Person();
-        person.setAge(12);
-        person.setName("dcw");
-        System.out.println(person.getName());
-        System.out.println(person.getAge());
-        Employee employee = new Employee();
-        employee.setSalary(123);
-        System.out.println(employee.getSalary());
-
+//        Lesson1 l1 = new Lesson1();
+//        l1.section1();
+//        dz1 dz = new dz1();
+//        Person person = new Person();
+//        person.setAge(12);
+//        person.setName("dcw");
+//        System.out.println(person.getName());
+//        System.out.println(person.getAge());
+//        Employee employee = new Employee();
+//        employee.setSalary(123);
+//        System.out.println(employee.getSalary());
+//        L3 l3 = new L3();
+//        l3.section3();
+        Transport car = new Car();
+        Transport bike = new Bike();
+        TransportManager tm = new TransportManager();
+        tm.startTransport(car);
+        tm.startTransport(bike);
     }
 }
